c101632c9fc34b37adfe9ba0fe22c6f6fe62c50b
imradhe <radhesyam.musicals@gmail.com>
2024-08-08 22:00:29 +0530

    Modified youtube downloader

--- code/YoutubeDownloadscipt.py
+++ code/YoutubeDownloadscipt.py
@@ -1,3 +1,4 @@
+import os
 import yt_dlp
 
 def download_audio_from_youtube(video_url, output_path='.', ffmpeg_location=None):
@@ -10,6 +11,7 @@ def download_audio_from_youtube(video_url, output_path='.', ffmpeg_location=None
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
             }],
+            'extract_flat': True,  # Flatten the playlist and download all videos
         }
 
         if ffmpeg_location:
@@ -21,17 +23,21 @@ def download_audio_from_youtube(video_url, output_path='.', ffmpeg_location=None
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# List of YouTube video URLs
-video_urls = [
-    'https://www.youtube.com/watch?v=E53GuZ8NFQw&ab_channel=BrajaBeats',
-    'https://www.youtube.com/watch?v=tZxnilHN8EE&ab_channel=GaanasampadaDevotional',
-    'https://www.youtube.com/watch?v=uYxt86Yk58w&ab_channel=Release-Topic',
-    'https://www.youtube.com/watch?v=WITUOwi3EYk'
-]
+# Dictionary of YouTube video URLs and corresponding directory names
+video_urls = {
+    'https://www.youtube.com/watch?v=tZxnilHN8EE&ab_channel=GaanasampadaDevotional': 'Dr. Vidyabhushana',
+    'https://www.youtube.com/watch?v=E53GuZ8NFQw&ab_channel=BrajaBeats': 'ISCKON',
+    'https://www.youtube.com/watch?v=WITUOwi3EYk': 'Udaya Shreyas',
+    'https://www.youtube.com/watch?v=C5UHgA9C3kw': 'Kum. Aditi',
+    'https://www.youtube.com/playlist?list=OLAK5uy_lOp1HGU8md9RbTmZfbR7Dc8LaBv-mAtug': 'Swami Paramarthananda',
+    'https://www.youtube.com/watch?v=hHq14qUebOc': 'Radha Gopinath Prabhu',
+}
 
 # Path to ffmpeg (if needed)
 ffmpeg_path = 'C:/ffmpeg/bin'  # Replace with the actual path if necessary
 
-# Download audio for each video URL
-for url in video_urls:
-    download_audio_from_youtube(url, ffmpeg_location=ffmpeg_path)
+# Download audio for each video URL and save in the corresponding directory
+for url, directory in video_urls.items():
+    output_path = os.path.join('.', directory)
+    os.makedirs(output_path, exist_ok=True)
+    download_audio_from_youtube(url, output_path=output_path, ffmpeg_location=ffmpeg_path)
